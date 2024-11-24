a0426b5af7e8c6fd9f529a701bcb861dbc8d6c82
Alex Berg <chexxor@gmail.com>
2024-11-23 19:04:48 -0600

    7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
 import partytown from '@astrojs/partytown';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), partytown()],
   output: 'server',
-  adapter: netlify(),
+  adapter: cloudflare()
 });
