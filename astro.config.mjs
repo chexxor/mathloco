cc61da1f5c7715f09663db521fae311a48c3a431
Alex Berg <chexxor@gmail.com>
2024-11-23 19:11:25 -0600

    8

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,9 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [tailwind(), sitemap(), partytown()],
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  })
 });
